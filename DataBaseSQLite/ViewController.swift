c6c21ca9112322c173794c7b9fd21be4fee43402
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-04-02 19:30:53 +0300

    Initial Commit

---
+++ DataBaseSQLite/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DataBaseSQLite
+//
+//  Created by Артем on 4/2/19.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
