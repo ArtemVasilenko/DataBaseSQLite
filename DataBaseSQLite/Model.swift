8d3f5d5b1144f7384ba77b6ab303faa9e34f27b3
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-04-05 13:17:50 +0300

    +

--- DataBaseSQLite/Model.swift
+++ DataBaseSQLite/Model.swift
@@ -8,8 +8,14 @@ protocol DB {
     func createTableInDB(db: OpaquePointer, newTable: String)
     func insertInTable(db: OpaquePointer, inTable: String, name: String)
     func updateTableWithGuard(db: OpaquePointer, inTable: String, name: String, id: String)
+    func selectFromTable(db: OpaquePointer, name: String, inTable: String, afterWhere: String) -> [String]
+    func deleteInTable(db: OpaquePointer, inTable: String, id: String)
+    func dropTable(db: OpaquePointer, inTable: String)
+    func getListTable(db: OpaquePointer) -> [String]
+    func anySelect(db: OpaquePointer, query: String) -> [String]
+    func closeDB(db: OpaquePointer)
+    
 }
-
 
 extension DB {
     
@@ -82,5 +88,119 @@ extension DB {
                 return
         }
         print("update whith guard done")
+        sqlite3_finalize(update)
+        
+    }
+    
+    func selectFromTable(db: OpaquePointer, name: String, inTable: String, afterWhere: String) -> [String] {
+        var values = [String]()
+        var str: OpaquePointer? = nil
+        var query = "SELECT \(name) FROM \(inTable) "
+        
+        if afterWhere != "" {
+            query += "WHERE \(afterWhere)"
+        }
+        
+        if sqlite3_prepare_v2(db, query, -1, &str, nil) == SQLITE_OK {
+            print("query \(query) is DONE")
+        } else {
+            print("query \(query) is uncorrect")
+        }
+        
+        while (sqlite3_step(str)) == SQLITE_ROW {
+            let id = sqlite3_column_int(str, 0)
+            let name = String(cString: sqlite3_column_text(str, 1))
+            values.append(String(id) + " " + name)
+        }
+        
+        return values
+    }
+    
+    func deleteInTable(db: OpaquePointer, inTable: String, id: String) {
+        var del: OpaquePointer? = nil
+        let query = "DELETE FROM \(inTable) WHERE ID = \(id)"
+        
+        guard sqlite3_prepare_v2(db, query, -1, &del, nil) == SQLITE_OK else {
+            print("error prepare deleting")
+            return
+        }
+        print("Prepare delete done")
+        
+        guard sqlite3_step(del) == SQLITE_DONE else {
+            print("error deleting")
+            return
+        }
+        print("Delete done")
+        sqlite3_finalize(del)
+        
+    }
+    
+    func dropTable(db: OpaquePointer, inTable: String) {
+        var drop4ik: OpaquePointer? = nil
+        let query = "DROP TABLE \(inTable)"
+        
+        guard sqlite3_prepare_v2(db, query, -1, &drop4ik, nil) == SQLITE_OK else {
+            print("error preape drop")
+            return
+        }
+        print("prepare drop done")
+        
+        guard sqlite3_step(drop4ik) == SQLITE_DONE else {
+            print("error drop")
+            return
+        }
+        print("drop done")
+        sqlite3_finalize(drop4ik)
+        
+    }
+    
+    func getListTable(db: OpaquePointer) -> [String] {
+        var values = [String]()
+        var str: OpaquePointer? = nil
+        let query = "select name from sqlite_master where type = 'table' and name <> 'sqlite_sequence'"
+        
+        if sqlite3_prepare_v2(db, query, -1, &str, nil) == SQLITE_OK {
+            print("query is DONE")
+        } else {
+            print("query is uncorrect")
+        }
+        
+        while (sqlite3_step(str)) == SQLITE_ROW {
+            let name = String(cString: sqlite3_column_text(str, 0))
+            values.append(name)
+        }
+        
+        return values
+    }
+    
+    func anySelect(db: OpaquePointer, query: String) -> [String] {
+        var values = [String]()
+        var str: OpaquePointer? = nil
+        
+        
+        if sqlite3_prepare_v2(db, query, -1, &str, nil) == SQLITE_OK {
+            print("Any query is DONE")
+        } else {
+            print("any guery is uncorrect")
+        }
+        
+        while (sqlite3_step(str)) == SQLITE_ROW {
+            var value = ""
+            
+            for i in 0..<sqlite3_column_count(str) {
+                let name = String(cString: sqlite3_column_text(str, i))
+                value += " " + name
+            }
+            values.append(value)
+            
+            
+        }
+        
+        
+        return values
+    }
+    
+    func closeDB(db: OpaquePointer) {
+        sqlite3_close(db)
     }
 }
--- DataBaseSQLite/ViewController.swift
+++ DataBaseSQLite/ViewController.swift
@@ -1,10 +1,11 @@
 import UIKit
-import SQLite3
+//import SQLite3
 
 class ViewController: UIViewController, DB {
     
     var fm = FileManager.default
     var url: URL?
+    let table = "MyTableFromXCodeGuard"
     var nameDB = "DataBase_From_XCode_Project.db"
     var db: OpaquePointer?
     let createMyTable = #"""
@@ -13,7 +14,14 @@ class ViewController: UIViewController, DB {
         "Name"    TEXT NOT NULL UNIQUE
         );
         """#
-
+    
+    let createMyTable2 = #"""
+        CREATE TABLE "MyTable" (
+        "ID"    INTEGER PRIMARY KEY AUTOINCREMENT,
+        "Name"    TEXT NOT NULL UNIQUE
+        );
+        """#
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -21,13 +29,25 @@ class ViewController: UIViewController, DB {
         removeDB(url: url!, fm: fm)
         db = createDataBase(url: url!)
         createTableInDB(db: db!, newTable: createMyTable)
-        insertInTable(db: db!, inTable: "MyTableFromXCodeGuard", name: "Ignat")
-        insertInTable(db: db!, inTable: "MyTableFromXCodeGuard", name: "Holodov")
-    
-        updateTableWithGuard(db: db!, inTable: "MyTableFromXCodeGuard", name: "Hi Holodov", id: "2")
+        createTableInDB(db: db!, newTable: createMyTable2)
         
-        sqlite3_close(db)
+        insertInTable(db: db!, inTable: self.table, name: "Ignat")
+        insertInTable(db: db!, inTable: self.table, name: "Holodov")
         
+        updateTableWithGuard(db: db!, inTable: self.table, name: "Hi Holodov", id: "2")
+        
+        print(selectFromTable(db: db!, name: "*", inTable: self.table, afterWhere: ""))
+        
+        deleteInTable(db: db!, inTable: self.table, id: "1")
+        
+        print(selectFromTable(db: db!, name: "*", inTable: self.table, afterWhere: ""))
+        
+        print(getListTable(db: db!))
+        
+        print(anySelect(db: db!, query: "SELECT * FROM \(self.table)"))
+        
+        //        dropTable(db: db!, inTable: self.table)
+        closeDB(db: db!)
     }
 }
 
